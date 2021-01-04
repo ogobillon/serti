6d56438e13cc41f5976e141e96999c24c995abeb
ogobillon <ogobillon@gmail.com>
2021-01-04 17:58:44 -0300

    ok

--- bottle_app.py
+++ bottle_app.py
@@ -1,15 +1,15 @@
-from bottle import route, run, static_file, error, template, TEMPLATES
+from bottle import route, run, static_file, error, template, TEMPLATES, request, post
 import db
-
+import corre
 
 #pythonanywhere.com
-root_ = '/home/ogobillon/mysite/'
-consultas = db.db_get("ogobillon.mysql.pythonanywhere-services.com","ogobillon","m7121941","ogobillon$serti_db")
+# root_ = '/home/ogobillon/mysite/'
+# consultas = db.db_get("ogobillon.mysql.pythonanywhere-services.com","ogobillon","m7121941","ogobillon$serti_db")
 
 
 #local
-#root_ = ''
-#consultas = db.db_get("localhost","root","1942","ogobillon$serti_db")
+root_ = ''
+consultas = db.db_get("localhost","root","1942","ogobillon$serti_db")
 
 root_html = root_ + 'static/html'
 root_img = root_ + 'static/img'
@@ -22,10 +22,20 @@ datos = {'f':consultas}
 def index():
     return static_file('index.html', root=root_html)
 
-
 @route('/ventas.html')
-def ventas():
-    return static_file('ventas.html', root = root_html)
+def ventas(name='World'):
+    TEMPLATES.clear()
+    return template(root_views + '/ventas.html')
+
+@route('/servicios.html')
+def servicios(name='World'):
+    TEMPLATES.clear()
+    return template(root_views + '/servicios.html', data=datos)
+
+@route('/enviado.html')
+def servicios(name='World'):
+    TEMPLATES.clear()
+    return template(root_views + '/enviado.html')
 
 @route('/img/<filepath:path>')
 def imagenes(filepath):
@@ -39,10 +49,13 @@ def estilos(filepath):
 def error404(error):
     return 'Nothing here, jsorry'
 
-@route('/servicios.html')
-def hello(name='World'):
-    TEMPLATES.clear()
-    return template(root_views + '/servicios.html', data=datos)
 
+@post('/c4g') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('user_name')
+    u_email = request.forms.get('user_mail')
+    mensaje = request.forms.get('user_message')
+    check = corre.send_email(username, u_email, mensaje)
+    return "<h1>enviado<h1>"
 
-#run(host='localhost', port=8080)
+run(host='localhost', port=8080)
---
+++ corre.py
@@ -0,0 +1,47 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+
+# User configuration
+def send_email(nombre, correo, mensaje):
+
+    sender_email = 'emailbottle@gmail.com'
+    receiver_email = 'ogobillon@gmail.com'
+    password = 'e7121941'
+    cuerpo = '''
+    NOMBRE: {}
+    EMAIL: {}
+    MSG:
+    {}'''.format(nombre,correo,mensaje)
+
+    msg = EmailMessage()
+    msg.set_content(cuerpo)
+
+    msg['Subject'] = 'serti pythonanywhere'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+
+
+
+    # Creating a SMTP session | use 587 with TLS, 465 SSL and 25
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # Encrypts the email
+    context = ssl.create_default_context()
+    server.starttls(context=context)
+    # We log in into our Google account
+    sesion = server.login(sender_email, password)
+    # Sending email from sender, to receiver with the email body
+    envios = server.send_message(msg)
+    print('Email sent!')
+
+    print('Closing the server...')
+    server.quit()
+
+    print('sesiondic: ', sesion)
+    print('envios dic', envios)
+
+    return None
+
+# send_email('juan', 'juan@perez.com', '13 mensaje de juan perez')
